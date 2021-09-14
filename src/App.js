f2d06660f8ea020dc9c7268d862b0d54865cfa94
Leonam Rodrigues <leonam_5000@hotmail.com>
2021-09-14 19:18:16 -0300

    Refatorando componentes de classe feito

--- src/App.js
+++ src/App.js
@@ -2,27 +2,48 @@ import { Component } from "react";
 
 import DisplayWeather from "./components/DisplayWeather";
 import SearchWeather from "./components/SearchWeather";
-
+import {useState} from "react"
 import "./App.css";
 
-class App extends Component {
-  state = {
-    weather: {},
-    city: "",
-  };
 
-  handleChangeWeather = (weatherData, city) => {
-    this.setState({ weather: weatherData, city: city });
-  };
+const App = () => {
+const [weather, setWeather] = useState({})
+const [city, setCity] = useState("")
 
-  render() {
-    return (
-      <div className="App">
-        <SearchWeather handleChangeWeather={this.handleChangeWeather} />
-        <DisplayWeather info={this.state} />
-      </div>
-    );
-  }
+const handleChangeWeather = (weatherData, city) => {
+  setWeather(weatherData)
+  setCity(city)
 }
 
+  return (
+    <div className="App">
+            <SearchWeather handleChangeWeather={handleChangeWeather} />
+            <DisplayWeather weather={weather} city = {city}/>
+    </div>
+  )
+}
 export default App;
+
+
+
+// class App extends Component {
+//   state = {
+//     weather: {},
+//     city: "",
+//   };
+
+//   handleChangeWeather = (weatherData, city) => {
+//     this.setState({ weather: weatherData, city: city });
+//   };
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <SearchWeather handleChangeWeather={this.handleChangeWeather} />
+//         <DisplayWeather info={this.state} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
--- src/components/SearchWeather/index.js
+++ src/components/SearchWeather/index.js
@@ -1,43 +1,76 @@
 import { Component } from "react";
-
+import {useEffect, useState} from "react"
 import "./style.css";
 
-class SearchWeather extends Component {
-  state = {
-    inputValue: "Curitiba",
-  };
+const SearchWeather = ({handleChangeWeather }) => {
+  const [inputValue, setInputValue ] = useState("Curitiba")
 
-  componentDidMount() {
-    this.handleSubmit();
+
+
+
+  const handleSubmit = () => {
+     fetch(`https://goweather.herokuapp.com/weather/${inputValue}`)
+     .then((response) => response.json())
+     .then((response) => handleChangeWeather(response, inputValue));
+  
   }
 
-  handleSubmit = () => {
-    const { inputValue } = this.state;
-    const { handleChangeWeather } = this.props;
 
-    fetch(`https://goweather.herokuapp.com/weather/${this.state.inputValue}`)
-      .then((response) => response.json())
-      .then((response) => handleChangeWeather(response, inputValue));
-  };
-
-  render() {
-    return (
-      <div className="search-container">
-        <div>
-          <input
-            className="search-input"
-            value={this.state.inputValue}
-            onChange={(e) => this.setState({ inputValue: e.target.value })}
-          />
-        </div>
-        <div>
-          <button className="search-button" onClick={this.handleSubmit}>
-            Search
-          </button>
-        </div>
-      </div>
-    );
-  }
+  return (
+          <div className="search-container">
+            <div>
+              <input
+                className="search-input"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+            </div>
+            <div>
+              <button className="search-button" onClick={handleSubmit}>
+                Search
+              </button>
+            </div>
+          </div>
+  )
 }
-
 export default SearchWeather;
+
+
+// class SearchWeather extends Component {
+//   state = {
+//     inputValue: "Curitiba",
+//   };
+
+//   componentDidMount() {
+//     this.handleSubmit();
+//   }
+
+//   handleSubmit = () => {
+//     const { inputValue } = this.state;
+//     const { handleChangeWeather } = this.props;
+
+//     fetch(`https://goweather.herokuapp.com/weather/${this.state.inputValue}`)
+//       .then((response) => response.json())
+//       .then((response) => handleChangeWeather(response, inputValue));
+//   };
+
+//   render() {
+//     return (
+//       <div className="search-container">
+//         <div>
+//           <input
+//             className="search-input"
+//             value={this.state.inputValue}
+//             onChange={(e) => this.setState({ inputValue: e.target.value })}
+//           />
+//         </div>
+//         <div>
+//           <button className="search-button" onClick={this.handleSubmit}>
+//             Search
+//           </button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
